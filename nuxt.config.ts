75b831ac3a4ae03314432dc6f8969c436c6c8506
Dmitry <dmytro.oborskiy@angleto.com>
2025-09-14 23:32:03 +0300

    feat: base styles, typography

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,6 +1,33 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
   compatibilityDate: '2025-07-15',
-  devtools: { enabled: true },
-  modules: ['@nuxt/eslint', '@nuxt/image', '@nuxt/scripts']
+  devtools: {
+    enabled: true,
+
+    timeline: {
+      enabled: true
+    }
+  },
+  modules: ['@nuxt/eslint', '@nuxt/image', '@nuxt/scripts'],
+  css: ['modern-normalize/modern-normalize.css', '~/assets/styles/main.scss'],
+  app: {
+    head: {
+      link: [
+        {
+          rel: 'preload',
+          as: 'font',
+          href: '/fonts/RoobertTRIAL-Regular.otf',
+          type: 'font/otf',
+          crossorigin: 'anonymous'
+        },
+        {
+          rel: 'preload',
+          as: 'font',
+          href: '/fonts/RoobertMonoTRIAL-Medium.otf',
+          type: 'font/otf',
+          crossorigin: 'anonymous'
+        }
+      ]
+    }
+  }
 })
