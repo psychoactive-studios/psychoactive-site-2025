875b4370d5e76d962c8a584a7d5c080b917057de
Dmitry <dmytro.oborskiy@angleto.com>
2025-09-15 17:42:37 +0300

    update: eslint rules

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,6 +1,16 @@
 // @ts-check
-import withNuxt from './.nuxt/eslint.config.mjs'
+import withNuxt from './.nuxt/eslint.config.mjs';
 
-export default withNuxt(
-  // Your custom configs here
-)
+export default withNuxt({
+  rules: {
+    'no-undef': 'error',
+    'vue/html-self-closing': [
+      'error',
+      {
+        html: {
+          void: 'any',
+        },
+      },
+    ],
+  },
+});
