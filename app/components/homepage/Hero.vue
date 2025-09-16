259e95448da5bde69d3285df50cbd454d9912b77
Dmitry <dmytro.oborskiy@angleto.com>
2025-09-16 19:16:15 +0300

    feat(Homepage): added hero top and bottom text

--- app/components/homepage/Hero.vue
+++ app/components/homepage/Hero.vue
@@ -1,6 +1,7 @@
 <script setup lang="ts">
 import { partnersData } from '~/data/partnersData';
 import Circle from '../ui/Circle.vue';
+import PlusIcon from '~/assets/icons/plus.svg';
 </script>
 
 <template>
@@ -9,6 +10,20 @@ import Circle from '../ui/Circle.vue';
       <section class="hero__intro_wrapper">
         <div class="scene">
           <Circle class="circle" />
+          <div class="psychoactive">
+            psychoactive®
+            <PlusIcon class="psychoactive__icon psychoactive_icon--right" />
+            <PlusIcon class="psychoactive__icon psychoactive_icon--bottom" />
+          </div>
+          <div class="top-text">
+            <img class="top-text__label" src="/img/Webflow_label.svg" alt="" />
+            <div class="top-text__agency">Digital First design agency</div>
+            <div class="top-text__innovation grey-text">Innovate</div>
+          </div>
+          <div class="bottom-text">
+            <div class="bottom-text__imagine grey-text">Imagine</div>
+            <div class="bottom-text__scale">scale</div>
+          </div>
         </div>
       </section>
       <section class="hero__intro_partners">
@@ -28,6 +43,7 @@ import Circle from '../ui/Circle.vue';
 <style lang="scss" scoped>
 @use '~/assets/styles/mixins' as *;
 @use '~/assets/styles/functions' as *;
+@use '~/assets/styles/variables' as *;
 .hero {
   &__intro {
     height: 100dvh;
@@ -42,12 +58,84 @@ import Circle from '../ui/Circle.vue';
         border: 2px dashed white(40);
         width: 100%;
         position: relative;
+        .psychoactive {
+          position: absolute;
+          top: 0;
+          left: 0;
+          font-family: 'RoobertMono', sans-serif;
+          color: white(50);
+          font-size: 18px;
+          font-style: normal;
+          line-height: 1;
+          text-transform: uppercase;
+          padding: 0 48px 48px 0;
+          &__icon {
+            position: absolute;
+            width: 7px;
+            height: 7px;
+            stroke: white(50);
+          }
+          .psychoactive_icon--right {
+            top: 0;
+            right: 0;
+          }
+          .psychoactive_icon--bottom {
+            bottom: 0;
+            left: 0;
+          }
+        }
+        .grey-text {
+          color: $color-grey;
+          font-size: 4.79vw;
+          font-style: normal;
+          font-weight: 400;
+          line-height: 88%;
+          letter-spacing: -0.2874vw;
+        }
         .circle {
           position: absolute;
           top: 50%;
           left: 50%;
           transform: translate(-50%, -50%) rotate(30deg);
           width: 50%;
+        }
+        .top-text {
+          position: absolute;
+          top: 0;
+          right: 0;
+          text-align: right;
+          &__label {
+            width: auto;
+            height: 48px;
+          }
+          &__agency {
+            font-family: 'RoobertMono', sans-serif;
+            color: white(80);
+            font-size: 18px;
+            font-style: normal;
+            line-height: 1;
+            text-transform: uppercase;
+            margin: 24px 0 32px 0;
+          }
+          &__innovation {
+            line-height: 65%;
+          }
+        }
+        .bottom-text {
+          position: absolute;
+          bottom: 0;
+          left: 0;
+          &__imagine {
+            line-height: 77%;
+          }
+          &__scale {
+            color: $color-foreground;
+            font-size: 12.6vw;
+            font-style: normal;
+            font-weight: 400;
+            line-height: 77%;
+            letter-spacing: -0.756vw;
+          }
         }
       }
     }
