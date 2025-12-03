c376fbe2abd649f77b5d2bb8c904fcc21f75c70a
Dmitry <scooterok@gmail.com>
2025-12-03 14:05:27 +0200

    feat(Services): add on-scroll text section and enable scroll smoother

--- app/pages/services.vue
+++ app/pages/services.vue
@@ -1,9 +1,53 @@
 <script setup lang="ts">
 import Hero from '~/components/services/Hero.vue';
+import OnScrollFilledText from '~/components/ui/OnScrollFilledText.vue';
+import useScrollSmoother from '~/composables/useScrollSmoother';
+
+const { enableScroll } = useScrollSmoother();
+
+onMounted(() => {
+  setTimeout(() => {
+    enableScroll();
+  }, 100);
+});
 </script>
 
 <template>
   <main class="services">
     <Hero />
+    <div class="container">
+      <section class="services__onscroll-text">
+        <OnScrollFilledText>
+          <span class="dark">Our work lives at the intersection of</span> art,
+          technology <span class="dark">and</span> strategy.
+          <span class="dark">From world-class</span>
+          creative direction <span class="dark">to</span> custom code
+          <span class="dark">and</span> enterprise-grade Webflow development,
+          <span class="dark"
+            >we help ambitious brands evolve, scale and lead with
+            confidence.</span
+          >
+        </OnScrollFilledText>
+      </section>
+    </div>
   </main>
 </template>
+
+<style scoped lang="scss">
+@use '~/assets/styles/mixins' as *;
+.services {
+  &__onscroll-text {
+    @include flex-center;
+    min-height: 100dvh;
+    max-width: 57vw;
+    margin: auto;
+    &:deep(.text-block) {
+      &::before {
+        content: '';
+        display: inline-block;
+        width: 10vw;
+      }
+    }
+  }
+}
+</style>
