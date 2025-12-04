066c32fda8563a2bd4c36c6abe92d59d8feb2000
Dmitry <scooterok@gmail.com>
2025-12-04 12:08:24 +0200

    feat(Services): implement video teleport feature and service list display

---
+++ app/components/services/FixedTeleportsTarget.vue
@@ -0,0 +1,20 @@
+<template>
+  <div id="services-list-video-teleport" />
+</template>
+<style scoped lang="scss">
+.services-list__video {
+  position: fixed;
+  top: 50%;
+  left: 50%;
+  transform: translate(-50%, -50%);
+  width: 40%;
+  aspect-ratio: 1;
+  margin: auto;
+  video {
+    width: 100%;
+    height: 100%;
+    object-fit: cover;
+    clip-path: circle(50% at 50% 50%);
+  }
+}
+</style>
---
+++ app/components/services/ServicesList.vue
@@ -0,0 +1,119 @@
+<script setup>
+import { listData } from '~/data/servicesData';
+
+onMounted(() => {});
+</script>
+
+<template>
+  <div class="services-list">
+    <ClientOnly>
+      <Teleport to="#services-list-video-teleport">
+        <div class="services-list__video">
+          <video
+            class="services-list__video_player"
+            src="/video/preview_reel.mp4"
+            autoplay
+            loop
+            muted
+            playsinline
+          />
+        </div>
+      </Teleport>
+    </ClientOnly>
+    <div class="container">
+      <ul class="services-list__items">
+        <li
+          v-for="item in listData"
+          :key="item.title"
+          class="services-list__item"
+        >
+          <h3 class="services-list__item_title">{{ item.title }}</h3>
+          <div class="services-list__item_divider">
+            <span class="line" />
+          </div>
+          <div class="services-list__item_description">
+            {{ item.description }}
+          </div>
+        </li>
+      </ul>
+    </div>
+  </div>
+</template>
+
+<style scoped lang="scss">
+@use '~/assets/styles/functions' as *;
+@use '~/assets/styles/variables' as *;
+.services-list {
+  &__video {
+    width: 40%;
+    aspect-ratio: 1;
+    margin: auto;
+    position: fixed;
+    top: 50%;
+    left: 50%;
+    transform: translate(-50%, -50%);
+    video {
+      width: 100%;
+      height: 100%;
+      object-fit: cover;
+      clip-path: circle(50% at 50% 50%);
+    }
+  }
+  &__items {
+    display: flex;
+    flex-direction: column;
+    gap: 423px;
+  }
+  &__item {
+    display: flex;
+    gap: getRem(24);
+    &_title {
+      font-family: 'RoobertMono';
+      font-size: 1rem;
+      font-style: normal;
+      font-weight: 500;
+      line-height: 100%;
+      text-transform: uppercase;
+      white-space: nowrap;
+      margin-top: 0.4rem;
+    }
+    &_divider {
+      flex-grow: 1;
+      .line {
+        display: block;
+        height: 1px;
+        background: rgba(217, 217, 217, 0.2);
+        position: relative;
+        will-change: width;
+        mix-blend-mode: difference;
+        margin-top: 0.9rem;
+        &::before,
+        &::after {
+          content: '';
+          position: absolute;
+          width: 7px;
+          height: 7px;
+          background-color: $color-dots;
+          border-radius: 50%;
+        }
+        &::before {
+          top: -3px;
+          left: -3px;
+        }
+        &::after {
+          top: -3px;
+          right: -4px;
+        }
+      }
+    }
+    &_description {
+      font-size: 24px;
+      font-style: normal;
+      font-weight: 400;
+      line-height: 30px; /* 125% */
+      color: white(80);
+      width: 380px;
+    }
+  }
+}
+</style>
