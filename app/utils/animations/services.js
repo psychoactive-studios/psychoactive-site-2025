a30b6f81608b569ad1d721e42f2d2a501408ea66
Dmitry <scooterok@gmail.com>
2025-12-09 19:33:13 +0200

    feat(Services): Services list mobile

--- app/utils/animations/services.js
+++ app/utils/animations/services.js
@@ -13,111 +13,135 @@ export const heroInitAnimation = (ctx, scrollSmoother) => {
   const duration1 = 2;
   const easeCircle = 'power3.inOut';
 
+  let matchMedia = gsap.matchMedia();
   ctx.add(() => {
-    gsap
-      .timeline({ id: 'services-initial-animation' })
-      .from(
-        '.services-3d-scene--wrapper',
-        {
-          scale: 0,
-          duration: duration1,
-          ease: ease1,
-        },
-        'firstPart'
-      )
-      .from(
-        '.center',
-        { scaleX: 0, ease: ease1, duration: duration1 },
-        'firstPart+=0.4'
-      )
-      .from(
-        '.circle-path-1, .circle-path-2',
-        {
-          strokeDashoffset: 626.43,
-          duration: 1.85,
-          ease: easeCircle,
-        },
-        'firstPart+=0.3'
-      )
-      .from(
-        '.circle-dots',
-        { autoAlpha: 0, rotate: 0, duration: 1.85, ease: easeCircle },
-        'firstPart+=0.3'
-      )
-      .add('secondPart', 'firstPart+=1.2')
-      .to(
-        'h1.title .char-center',
-        {
-          duration: 3.2,
-          scrambleText: {
-            text: '{original}',
-            chars: 'uppercase',
-            tweenLength: false,
-          },
-        },
-        'secondPart'
-      )
-      .fromTo(
-        'h1.title .char-center',
-        { opacity: 0, visibility: 'hidden' },
-        {
-          opacity: 1,
-          visibility: 'visible',
-          duration: 0.01,
-          stagger: {
-            amount: 0.85,
-            from: 'random',
-          },
-        },
-        'secondPart'
-      )
-      .fromTo(
-        '.bottom-text__web, .bottom-text__web-svg',
-        { xPercent: -175, visibility: 'hidden' },
-        {
-          xPercent: 0,
-          visibility: 'visible',
-          duration: 1.15,
-          ease: 'power4.out',
-        },
-        'secondPart'
-      )
-      .from(
-        '.bottom-text__shape .char-center, .top-text .char-center',
-        {
-          opacity: 0,
-          duration: 0.01,
-          stagger: {
-            amount: 0.4,
-            from: 'random',
-          },
-        },
-        'secondPart'
-      )
-      .fromTo(
-        '.right-label, .top-text__label',
-        { xPercent: 175, visibility: 'hidden' },
-        {
-          xPercent: 0,
-          visibility: 'visible',
-          duration: 1.15,
-          ease: 'power4.out',
-        },
-        'secondPart'
-      )
-      .to(
-        layoutElements,
-        {
-          scale: 1,
-          opacity: 1,
-          duration: 0.75,
-          ease: 'power3.out',
-        },
-        'secondPart+=0.2'
-      )
-      .add(() => {
-        scrollSmoother.value.paused(false);
-      }, '-=1');
+    matchMedia.add(
+      {
+        isDesktop: `(min-width: 769px)`,
+        isMobile: `(max-width: 768px)`,
+      },
+      (context) => {
+        let { isMobile } = context.conditions;
+        const timeline = gsap
+          .timeline({ id: 'services-initial-animation' })
+          .from(
+            '.services-3d-scene--wrapper',
+            {
+              scale: 0,
+              duration: duration1,
+              ease: ease1,
+            },
+            'firstPart'
+          )
+          .from(
+            '.center',
+            { scaleX: 0, ease: ease1, duration: duration1 },
+            'firstPart+=0.4'
+          )
+          .from(
+            '.circle-path-1, .circle-path-2',
+            {
+              strokeDashoffset: 626.43,
+              duration: 1.85,
+              ease: easeCircle,
+            },
+            'firstPart+=0.3'
+          )
+          .from(
+            '.circle-dots',
+            { autoAlpha: 0, rotate: 0, duration: 1.85, ease: easeCircle },
+            'firstPart+=0.3'
+          )
+          .add('secondPart', 'firstPart+=1.2')
+          .to(
+            'h1.title .char-center',
+            {
+              duration: 3.2,
+              scrambleText: {
+                text: '{original}',
+                chars: 'uppercase',
+                tweenLength: false,
+              },
+            },
+            'secondPart'
+          )
+          .fromTo(
+            'h1.title .char-center',
+            { opacity: 0, visibility: 'hidden' },
+            {
+              opacity: 1,
+              visibility: 'visible',
+              duration: 0.01,
+              stagger: {
+                amount: 0.85,
+                from: 'random',
+              },
+            },
+            'secondPart'
+          )
+          .fromTo(
+            '.bottom-text__web, .bottom-text__web-svg',
+            { xPercent: -175, visibility: 'hidden' },
+            {
+              xPercent: 0,
+              visibility: 'visible',
+              duration: 1.15,
+              ease: 'power4.out',
+            },
+            'secondPart'
+          )
+          .from(
+            '.bottom-text__shape .char-center, .top-text .char-center',
+            {
+              opacity: 0,
+              duration: 0.01,
+              stagger: {
+                amount: 0.4,
+                from: 'random',
+              },
+            },
+            'secondPart'
+          )
+          .fromTo(
+            '.right-label, .top-text__label',
+            { xPercent: 175, visibility: 'hidden' },
+            {
+              xPercent: 0,
+              visibility: 'visible',
+              duration: 1.15,
+              ease: 'power4.out',
+            },
+            'secondPart'
+          )
+          .to(
+            layoutElements,
+            {
+              scale: 1,
+              opacity: 1,
+              duration: 0.75,
+              ease: 'power3.out',
+            },
+            'secondPart+=0.2'
+          );
+        if (isMobile) {
+          timeline
+            /* ======= Layout elements part mobile ========= */
+            .fromTo(
+              document.querySelector('.navigation-mobile'),
+              { yPercent: 200 },
+              {
+                yPercent: 0,
+                duration: 1,
+                ease: 'power3.out',
+              },
+              'secondPart+=0.2'
+            );
+        }
+        timeline.add(() => {
+          scrollSmoother.value.paused(false);
+        }, '-=1');
+      }
+    );
   });
 };
 
@@ -225,82 +249,91 @@ export const servicesListAnimation = (ctx, root) => {
       charsClass: 'char-center',
     }
   );
+
+  let matchMedia = gsap.matchMedia();
+
   ctx.add(() => {
-    gsap.to(document.querySelector('.services-list__video_player'), {
-      clipPath: 'circle(50% at 50% 50%)',
-      duration: 1,
-      scrollTrigger: {
-        trigger: root,
-        start: 'top center',
-        end: () => `${window.innerHeight * 0.5} center`,
-        scrub: true,
+    matchMedia.add(
+      {
+        isDesktop: `(min-width: 768px)`,
       },
-    });
-
-    gsap.utils.toArray('.services-list__item').forEach((item) => {
-      const title = item.querySelectorAll(
-        '.services-list__item_title .char-center'
-      );
-      const description = item.querySelectorAll(
-        '.services-list__item_description  .char-center'
-      );
-      const line = item.querySelector('.services-list__item_divider .line');
-
-      gsap
-        .timeline({
-          scrollTrigger: {
-            trigger: item,
-            start: 'top bottom',
-            end: 'bottom bottom',
-          },
-        })
-        .to(
-          title,
-          {
-            duration: 2.3,
-            scrambleText: {
-              text: '{original}',
-              chars: 'uppercase',
-              tweenLength: false,
+      (context) => {
+        let { isDesktop } = context.conditions;
+        if (isDesktop) {
+          gsap.to(document.querySelector('.services-list__video_player'), {
+            clipPath: 'circle(50% at 50% 50%)',
+            duration: 1,
+            scrollTrigger: {
+              trigger: root,
+              start: 'top center',
+              end: () => `${window.innerHeight * 0.5} center`,
+              scrub: true,
             },
-          },
-          '<+=0.2'
-        )
-        .from(
-          title,
-          {
-            opacity: 0,
-            duration: 0.01,
-            stagger: {
-              amount: 0.9,
-              from: 'random',
-            },
-          },
-          '<'
-        )
-        .from(line, { opacity: 0, duration: 0.5 }, '<+=0.5')
-        .fromTo(
-          line,
-          { width: '0%' },
-          { width: '100%', duration: 1.2, ease: 'power4.inOut' },
-          '<+=0.2'
-        )
-        .from(
-          description,
-          {
-            opacity: 0,
-            duration: 0.0001,
-            stagger: 0.015,
-          },
-          '-=0.75'
-        );
-    });
-    // ScrollTrigger.create({
-    //   trigger: '.services-list',
-    //   start: 'top center',
-    //   end: 'bottom center',
-    //   markers: true,
-    // });
+          });
+
+          gsap.utils.toArray('.services-list__item').forEach((item) => {
+            const title = item.querySelectorAll(
+              '.services-list__item_title .char-center'
+            );
+            const description = item.querySelectorAll(
+              '.services-list__item_description  .char-center'
+            );
+            const line = item.querySelector(
+              '.services-list__item_divider .line'
+            );
+
+            gsap
+              .timeline({
+                scrollTrigger: {
+                  trigger: item,
+                  start: 'top bottom',
+                  end: 'bottom bottom',
+                },
+              })
+              .to(
+                title,
+                {
+                  duration: 2.3,
+                  scrambleText: {
+                    text: '{original}',
+                    chars: 'uppercase',
+                    tweenLength: false,
+                  },
+                },
+                '<+=0.2'
+              )
+              .from(
+                title,
+                {
+                  opacity: 0,
+                  duration: 0.01,
+                  stagger: {
+                    amount: 0.9,
+                    from: 'random',
+                  },
+                },
+                '<'
+              )
+              .from(line, { opacity: 0, duration: 0.5 }, '<+=0.5')
+              .fromTo(
+                line,
+                { width: '0%' },
+                { width: '100%', duration: 1.2, ease: 'power4.inOut' },
+                '<+=0.2'
+              )
+              .from(
+                description,
+                {
+                  opacity: 0,
+                  duration: 0.0001,
+                  stagger: 0.015,
+                },
+                '-=0.75'
+              );
+          });
+        }
+      }
+    );
   });
 };
 
