0f8a1693c0b7087a4b772e20129d256a25347ce6
Dmitry <scooterok@gmail.com>
2025-12-22 15:17:09 +0200

    feat: Add new About page with Hero section featuring a video background and animated center line.

---
+++ app/pages/about.vue
@@ -0,0 +1,28 @@
+<script setup>
+import gsap from 'gsap';
+import Hero from '~/components/about/Hero.vue';
+import useScrollSmoother from '~/composables/useScrollSmoother';
+const { enableScroll } = useScrollSmoother();
+
+onMounted(() => {
+  const layoutElements = gsap.utils.toArray([
+    '#header-logo',
+    '#header-navigation-button',
+    '#header-sound-button',
+  ]);
+  setTimeout(() => {
+    gsap.to(
+      layoutElements,
+      { scale: 1, opacity: 1, duration: 0.75, ease: 'power3.out' },
+      '<+=1'
+    );
+    enableScroll();
+  }, 100);
+});
+</script>
+
+<template>
+  <main class="about">
+    <Hero />
+  </main>
+</template>
