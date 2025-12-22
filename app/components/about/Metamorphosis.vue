a4db78c7afd0900994afa1b936d99945704b58f5
Dmitry <scooterok@gmail.com>
2025-12-22 17:48:50 +0200

    feat: add Metamorphosis component to the about page, featuring an interactive button with scramble text animation.

---
+++ app/components/about/Metamorphosis.vue
@@ -0,0 +1,136 @@
+<script setup>
+import gsap from 'gsap';
+import useAudioManager from '~/composables/useAudioManager';
+
+const { playInteractionSound } = useAudioManager();
+
+const letsTalkButtonRef = ref(null);
+
+const handleHoverEffect = () => {
+  const el = letsTalkButtonRef.value;
+  // Stop any ongoing animations on this element
+  if (gsap.isTweening(el)) return;
+
+  // Set the width to prevent layout shift
+  const width = el.offsetWidth;
+  gsap.set(el, { width });
+
+  playInteractionSound();
+
+  // Store the original text
+  gsap.to(el, {
+    duration: 0.7,
+    ease: 'none',
+    scrambleText: {
+      text: '{original}',
+      chars: '0123456789!@#$%^&*()-_=+[]{};:<>/?,.',
+      tweenLength: false,
+    },
+    overwrite: true,
+    onComplete: () => {
+      gsap.set(el, { clearProps: 'all' });
+    },
+  });
+};
+</script>
+<template>
+  <section class="metamorphosis">
+    <div class="container">
+      <div class="metamorphosis__button">
+        <div class="metamorphosis__button_line">
+          <span class="line" />
+        </div>
+        <button class="button" @mouseenter="handleHoverEffect">
+          <span ref="letsTalkButtonRef">METAMORPHOSIS DEFINITION</span>
+          <span class="button__dots">
+            <i />
+            <i />
+            <i />
+          </span>
+        </button>
+        <div class="metamorphosis__button_line">
+          <span class="line" />
+        </div>
+      </div>
+    </div>
+  </section>
+</template>
+
+<style scoped lang="scss">
+@use '~/assets/styles/variables' as *;
+@use '~/assets/styles/functions' as *;
+.metamorphosis {
+  padding-bottom: 160px;
+  &__button {
+    display: grid;
+    grid-template-columns: 1fr auto 1fr;
+    align-items: center;
+    gap: 36px;
+    .button {
+      font-family: 'RoobertMono';
+      font-size: 1rem;
+      font-style: normal;
+      font-weight: 500;
+      line-height: 100%;
+      text-transform: uppercase;
+      padding: 12px 24px 12px 80px;
+      border: 1px solid white(20);
+      border-radius: 48px;
+      &__dots {
+        display: inline-flex;
+        gap: 2px;
+        margin-left: 60px;
+        i {
+          width: 6px;
+          height: 6px;
+          background-color: $color-foreground;
+          border-radius: 50%;
+          display: inline-block;
+        }
+      }
+      &:hover {
+        .button__dots {
+          i {
+            &:nth-child(1) {
+              animation: flicker-effect-3 0.5s forwards;
+            }
+            &:nth-child(2) {
+              animation: flicker-effect-3 0.5s forwards 0.2s;
+            }
+            &:nth-child(3) {
+              animation: flicker-effect-3 0.5s forwards 0.3s;
+            }
+          }
+        }
+      }
+    }
+    &_line {
+      .line {
+        display: block;
+        width: 100%;
+        height: 1px;
+        background: white(10);
+        position: relative;
+        will-change: width;
+        &::before,
+        &::after {
+          content: '';
+          position: absolute;
+          width: 7px;
+          height: 7px;
+          background-color: white(50);
+          border-radius: 50%;
+        }
+        &::before {
+          top: -3px;
+          left: -3px;
+        }
+        &::after {
+          top: -3px;
+          right: -4px;
+        }
+      }
+    }
+  }
+}
+</style>
