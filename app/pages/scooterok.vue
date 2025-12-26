44ae4307f3ce4a59ca3e4bfb1d9f5f02a5aca970
Dmitry <scooterok@gmail.com>
2025-12-26 13:31:57 +0200

    feat: Add ScooterOk component with SVG morphing and interactive dots; create scooterok page to utilize the component

---
+++ app/pages/scooterok.vue
@@ -0,0 +1,24 @@
+<script setup lang="ts">
+import ScooterOk from '~/components/ui/ScooterOk.vue';
+</script>
+
+<template>
+  <main class="scooterok">
+    <ScooterOk
+      shape="svg"
+      :svg-urls="['/img/webflow-logo.svg', '/img/logo_sky.svg']"
+      :morph-interval="5000"
+    />
+  </main>
+</template>
+
+<style scoped lang="scss">
+@use '~/assets/styles/mixins' as *;
+.scooterok {
+  @include flex-center;
+  height: 100vh;
+  & > * {
+    width: 50%;
+  }
+}
+</style>
