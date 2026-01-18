ac18f9815a760ee1ca4e28eea50fa10ec3bc6ec9
Dmitry <scooterok@gmail.com>
2026-01-18 15:16:08 +0200

    feat(accordion): add onToggle prop and integrate with ServicesAccordion component

--- app/components/webflow/Services.vue
+++ app/components/webflow/Services.vue
@@ -2,6 +2,7 @@
 import gsap from 'gsap';
 import ServicesAccordion from '../ui/ServicesAccordion.vue';
 import { SplitText } from 'gsap/SplitText';
+import LetsTalkScene from '../ui/LetsTalkScene.vue';
 
 defineProps({
   data: {
@@ -12,6 +13,7 @@ defineProps({
 
 const containerRef = ref(null);
 const accordionRef = ref(null);
+const sceneRef = ref(null);
 let ctx = null;
 
 onMounted(async () => {
@@ -54,23 +56,45 @@ onMounted(async () => {
         },
         'firstPart'
       )
-      .add(() => accordionRef.value.toggle(0), 'firstPart+=0.8');
+      .add(() => {
+        accordionRef.value.toggle(0);
+      }, 'firstPart+=0.8');
   }, containerRef.value);
 });
 
 onUnmounted(() => {
   if (ctx) ctx.revert();
 });
+
+const handleToggle = (index) => {
+  sceneRef.value.nextShape(index);
+};
 </script>
 
 <template>
   <div ref="containerRef" class="our-services">
     <div class="our-services__accordion">
       <h2 class="subheader--mobile">Full-service Webflow expertise</h2>
-      <ServicesAccordion ref="accordionRef" :list="data" />
+      <ServicesAccordion
+        ref="accordionRef"
+        :list="data"
+        @toggle="handleToggle"
+      />
     </div>
     <div class="our-services__media">
-      <img src="/img/test-webflow-our-services.jpg" alt="Our Services Image" />
+      <!-- <img src="/img/test-webflow-our-services.jpg" alt="Our Services Image" /> -->
+      <LetsTalkScene
+        ref="sceneRef"
+        class="our-services__media_scene"
+        :auto-play="false"
+        :svg-urls="[
+          '/img/frog_steps/frog01.svg',
+          '/img/frog_steps/frog02.svg',
+          '/img/frog_steps/frog03.svg',
+          '/img/frog_steps/frog04.svg',
+          '/img/frog_steps/frog05.svg',
+        ]"
+      />
     </div>
   </div>
 </template>
@@ -84,6 +108,7 @@ onUnmounted(() => {
   display: grid;
   grid-template-columns: 1fr 1fr;
   gap: getRem(24);
+  width: 100%;
   @include respond(mobile) {
     grid-template-columns: 1fr;
   }
@@ -105,17 +130,17 @@ onUnmounted(() => {
   }
   &__media {
     aspect-ratio: 1.31;
-    border-radius: getRem(10);
+    // border-radius: getRem(10);
     overflow: hidden;
-    background-color: $color-foreground;
+    // background-color: $color-foreground;
     @include respond(mobile) {
       order: 1;
     }
-    & > * {
-      width: 100%;
-      height: 100%;
-      object-fit: contain;
-    }
+    // & > * {
+    //   width: 100%;
+    //   height: 100%;
+    //   object-fit: contain;
+    // }
   }
 }
 </style>
