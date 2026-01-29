0bd7a0470c55640f0665dc3e5bb703f351ec3831
Dmitry <scooterok@gmail.com>
2026-01-29 18:11:37 +0200

    feat(Navigation): enhance loader animation logic and update navigation data structure

--- app/data/navigationData.js
+++ app/data/navigationData.js
@@ -1,7 +1,7 @@
 export const navigationData = [
-  { id: 'about', title: 'About', url: '/about' },
-  { id: 'work', title: 'Work', url: '/work' },
-  { id: 'services', title: 'Services', url: '/services' },
-  { id: 'webflow', title: 'Webflow', url: '/webflow' },
-  { id: 'content', title: 'Content', url: '/content-hub' },
+  { id: 'about', title: 'About', url: '/about', loader: true },
+  { id: 'work', title: 'Work', url: '/work', loader: false },
+  { id: 'services', title: 'Services', url: '/services', loader: true },
+  { id: 'webflow', title: 'Webflow', url: '/webflow', loader: true },
+  { id: 'content', title: 'Content', url: '/content-hub', loader: false },
 ];
