8156c9fee302207a428ecf1f64a5424967223d3a
Dmitry <scooterok@gmail.com>
2026-02-07 21:54:50 +0200

    fix(SetsUpAccordion): remove unused ButtonDotsArrow import for cleaner code fix(default): change opacity animation to scale for improved visual effect

--- app/components/ui/SetsUpAccordion.vue
+++ app/components/ui/SetsUpAccordion.vue
@@ -1,7 +1,6 @@
 <script setup>
 import gsap from 'gsap';
 import useAudioManager from '~/composables/useAudioManager';
-import ButtonDotsArrow from './ButtonDotsArrow.vue';
 
 const props = defineProps({
   list: {
